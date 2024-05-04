ab5bffacd8e86135f8c7a60f4482241b14da49ff
villads <74766251+villadsfrede@users.noreply.github.com>
2024-05-04 23:25:03 +0200

    Update telescope.lua

--- nvim/lua/villa/plugins/telescope.lua
+++ nvim/lua/villa/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
   },
 
   config = function()
-    local telescope = require('telescope'
+    local telescope = require('telescope')
     local actions = require('telescope.actions')
  
     telescope.setup({
